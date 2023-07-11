c2ee52dc3995eb2c0e704a0e722c777ccd6820f7
YagoHFA <yagof2421@gmail.com>
2023-07-10 22:17:53 -0300

    Inserts no H2

--- src/main/java/com/hyllcron/hfa/entities/Game.java
+++ src/main/java/com/hyllcron/hfa/entities/Game.java
@@ -13,25 +13,29 @@ public class Game {
     private String title;
     @Column(name = "game_year")
     private Integer year;
-    private String genere;
-    private String plataform;
-    private String imUrl;
-    private String shortDescripition;
+    private String genre;
+    private String platforms;
+    private double score;
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(long id, String title, Integer year, String genere, String plataform, String imUrl,
-                String shortDescripition, String longDescription) {
+    public Game(long id, String title, Integer year, String genre, String platforms, String imgUrl,
+                String shortDescription, String longDescription, double score) {
         this.id = id;
         this.title = title;
         this.year = year;
-        this.genere = genere;
-        this.plataform = plataform;
-        this.imUrl = imUrl;
-        this.shortDescripition = shortDescripition;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
 
@@ -59,36 +63,44 @@ public class Game {
         this.year = year;
     }
 
-    public String getGenere() {
-        return genere;
+    public String getGenre() {
+        return genre;
     }
 
-    public void setGenere(String genere) {
-        this.genere = genere;
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlataforms(String platforms) {
+        this.platforms = platforms;
     }
 
-    public String getImUrl() {
-        return imUrl;
+    public double getScore() {
+        return score;
     }
 
-    public void setImUrl(String imUrl) {
-        this.imUrl = imUrl;
+    public void setScore(double score) {
+        this.score = score;
     }
 
-    public String getShortDescripition() {
-        return shortDescripition;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
-    public void setShortDescripition(String shortDescripition) {
-        this.shortDescripition = shortDescripition;
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
     }
 
     public String getLongDescription() {
