bdd6c3664c021543d1f7b1309294169f461f0b63
YagoHFA <yagof2421@gmail.com>
2023-07-13 17:18:07 -0300

    Adicionando findById

--- src/main/java/com/hyllcron/hfa/Services/GameServices.java
+++ src/main/java/com/hyllcron/hfa/Services/GameServices.java
@@ -1,10 +1,13 @@
 package com.hyllcron.hfa.Services;
 
+import com.hyllcron.hfa.dto.GameDTO;
 import com.hyllcron.hfa.dto.GameMinDTO;
 import com.hyllcron.hfa.entities.Game;
 import com.hyllcron.hfa.repositories.GameRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -12,9 +15,16 @@ public class GameServices {
     @Autowired
     private GameRepositorie gameRepositorie;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
        var result = gameRepositorie.findAll();
        List<GameMinDTO> dto  = result.stream().map(x -> new GameMinDTO(x)).toList();
        return dto;
+    }
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId){
+        Game result = gameRepositorie.findById(gameId).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
     }
 }
